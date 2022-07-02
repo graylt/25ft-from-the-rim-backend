bbb9664f3a982e838b8bd2c42bdc9acca57fd04e
tricia gray <tgray@tricias-MacBook-Pro.local>
2022-07-02 03:18:14 -0400

    updating

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const cors = require('cors');
 const app = express();
 const path = require('path');
 const postgres = require('./postgres.js');
+const PORT = process.env.PORT || 5000;
 // const bcrypt = require('bcrypt');
 // const validInfo = require('./middleware/validInfo');
 // const authorize = require('./middleware/authorize');
@@ -21,19 +22,28 @@ require('dotenv').config();
 //Middleware
 //___________________
 
+app.use(cors());
 // app.use(methodOverride('_method'));
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
-app.use(cors());
 
 
 
 //Static
 // app.use(express.static('public'));
-app.use(express.static(__dirname + "/public"));
+// app.use(express.static(__dirname + "/public"));
 
-// Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, 'build')));
+// // Serve static files from the React frontend app
+// app.use(express.static(path.join(__dirname, 'build')));
+
+if (process.env.NODE_ENV === "production") {
+    //server static content
+    //npm run build
+    app.use(express.static(path.join(__dirname, "client/build")));
+  }
+  
+  console.log(__dirname);
+  console.log(path.join(__dirname, "client/build"));
  
 
 
@@ -98,8 +108,16 @@ postgres.connect();
 //     console.log(`server has started on port ${process.env.PORT || 3000}`)
 // })
 
-const port = process.env.PORT || 5000;
-app.listen(port, () => console.log("Server running on port 5000"));
+// const port = process.env.PORT || 5000;
+// app.listen(port, () => console.log("Server running on port 5000"));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client/build/index.html"));
+  });
+  
+  app.listen(PORT, () => {
+    console.log(`Server is starting on port ${PORT}`);
+  });
 
 //___________________
 //Listener
@@ -114,6 +132,8 @@ app.listen(port, () => console.log("Server running on port 5000"));
 
 // non-controller routes
 /////////////////////////////////////////////////////////////////////////
+
+// "test": "echo \"Error: no test specified\" && exit 1"
 
 //test csv player db route
 // app.get("/player", async (req, res) => {
