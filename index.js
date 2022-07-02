357b038659aa44081b590035dadb5a4e24383283
tricia gray <tgray@tricias-MacBook-Pro.local>
2022-07-01 22:50:56 -0400

    updated index file

--- index.js
+++ index.js
@@ -23,7 +23,18 @@ require('dotenv').config();
 app.use(express.json());
 app.use(cors());
 
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
+
+if (process.env.NODE_ENV === 'production') {
+    // Exprees will serve up production assets
+    app.use(express.static('client/build'));
+  
+    // Express serve up index.html file if it doesn't recognize route
+    const path = require('path');
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
 
 // app.use(methodOverride('_method'));
 app.use(express.urlencoded({extended: false}));
