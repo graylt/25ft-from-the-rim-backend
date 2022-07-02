24f6021b1f8c4e75c10e59ff6f3d7495eb7f415c
tricia gray <tgray@tricias-MacBook-Pro.local>
2022-07-02 00:02:22 -0400

    updating

--- index.js
+++ index.js
@@ -26,16 +26,13 @@ app.use(cors());
 app.use(express.static(path.join(__dirname, '..', 'build')));
 app.use(express.static('public'));
 
-if (process.env.NODE_ENV === 'production') {
-    // Exprees will serve up production assets
-    app.use(express.static('client/build'));
   
     // Express serve up index.html file if it doesn't recognize route
     const path = require('path');
     app.get('*', (req, res) => {
       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
-  }
+
 
 // app.use(methodOverride('_method'));
 app.use(express.urlencoded({extended: false}));
